3fcfce60883f5f9002b3417ad07d966dbf1660c9
Mantis <mjbgtaad56@gmail.com>
2025-09-23 14:45:17 -0400

    Set up project framework

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 
 import express from "express";
+import { createProxy } from "http-proxy";
 
 const app = express();
+const proxy = createProxy();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!!");
+app.get("/api/game/create", (req, res) => {
+    // TODO: create a game code and associate with the requester's IP
+});
+
+app.get("/api/game/heartbeat/:gameID", (req, res) => {
+    // TODO: keep gameID alive
+    // req.params.gameID
+});
+
+app.get("/api/game/connect/:gameID", (req, res) => {
+    // TODO: connect a websocket to a server
+    // req.params.gameID
 });
 
 app.listen(8080);
